dbc71aaac6e4838fb3aaf3a2cfc39d3b0a9ab40c
viplash4 <viplash4@gmail.com>
2026-03-05 12:44:13 +0200

    feat: add statuses changes

--- todo-cli.go
+++ todo-cli.go
@@ -131,8 +131,48 @@ func main() {
 				fmt.Printf("[#%d] [%s] %s \n", t.Id, t.Status, t.Description)
 			}
 		}
-
+	case os.Args[1] == "mark-in-progress":
+		if (len(os.Args) > 2){
+			tasks := loadTasks()
+			id, err := strconv.Atoi(os.Args[2])
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				
+				for i, t := range tasks {
+					if t.Id == id {
+						tasks[i].Status = "in progress"
+						tasks[i].UpdatedAt = time.Now()
+						saveTasks(tasks)
+						fmt.Printf("Task #%s was successfully updated!\n", os.Args[2])
+						break
+					}
+				}
+			}
+			
+		}
+	case os.Args[1] == "mark-done":
+		if (len(os.Args) > 2){
+			tasks := loadTasks()
+			id, err := strconv.Atoi(os.Args[2])
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				
+				for i, t := range tasks {
+					if t.Id == id {
+						tasks[i].Status = "done"
+						tasks[i].UpdatedAt = time.Now()
+						saveTasks(tasks)
+						fmt.Printf("Task #%s was successfully updated!\n", os.Args[2])
+						break
+					}
+				}
+			}
+			
+		}
     }
+	
 	
 		
 }
